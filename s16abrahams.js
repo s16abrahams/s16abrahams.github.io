1a822791840672571b6a237456f78894f65d22b6
s16abrahams <53270447+s16abrahams@users.noreply.github.com>
2019-07-24 15:38:15 -0400

    Create s16abrahams.js

---
+++ s16abrahams.js
@@ -0,0 +1,12 @@
+function myFunction(){
+  document.getElementById("Summer").innerHTML = "What are your plans this summer?";
+}
+function myFunction2(){
+  document.getElementById("Summer").innerHTML = "Not Sure... Yetttt!";
+}
+function myFunction3(){
+  document.getElementById("Summer").innerHTML = "Have you decided yet??!";
+}
+function myFunction4(){
+  document.getElementById("Summer").innerHTML = "GWC!!!";
+}
